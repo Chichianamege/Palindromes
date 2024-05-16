924d925c08295adfc47408b90ce415214a94d6bd
jake <130408715+Chichianamege@users.noreply.github.com>
2024-05-15 21:37:11 -0400

    Add files via upload
    1.	Push original string onto Stack_1 and also Stack_2
    2.	Pop Stack_1 and push onto Stack_3 until Stack_1 is empty
    3.	At this point Stack_2 is the original string and Stack_3 is the reverse
    4.	Compare Stack_2 and Stack_3 using dot =
    5.	If Stack_2 is equal to Stack_3 the string is a palindrome
    
    Test your string by entering data from the keyboard and writing out to the screen whether the string is a palindrome.

---
+++ dsassignment4part2.java
@@ -0,0 +1,49 @@
+// Name: Chidera Anamege
+// Class: CS 3305/ W01
+// Term: Spring 2024
+//	Instructor:  Carla McManus
+//	Assignment:  04-Part-2-Palindromes
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class dsassignment4part2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine().toLowerCase(); // converted to lowercase
+
+        if (isPalindrome(input)) { // this will check if the input string is a palindrome
+            System.out.println("Yes, the string is a palindrome."); // print a message letting the user know it's a palindrome
+        } else {
+            System.out.println("No, the string is not a palindrome."); // print a message letting the user know it's not a palindrome
+        }
+    }
+
+    public static boolean isPalindrome(String input) {
+        Stack<Character> stack1 = new Stack<>(); // create first stack to hold characters of the input string
+        Stack<Character> stack2 = new Stack<>(); // create second stack to hold characters of the input string
+        Stack<Character> stack3 = new Stack<>(); // create a third stack to hold characters of the input string
+
+        // push original string onto stack_1 and stack_2
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i); // get each character of the input string
+            stack1.push(ch); // push the character onto stack1
+            stack2.push(ch); // push the character onto stack2
+        }
+
+        // this will pop stack1 and push onto stack3 until stack1 is empty
+        while (!stack1.isEmpty()) {
+            stack3.push(stack1.pop()); // pop a character from stack1 and push onto stack3
+        }
+
+        // just to compare stack2 and stack3
+        while (!stack2.isEmpty() && !stack3.isEmpty()) {
+            if (!stack2.pop().equals(stack3.pop())) {
+                return false; // this lets the system know if any characters don't match, it's not a palindrome
+            }
+        }
+        return stack2.isEmpty() && stack3.isEmpty();
+    }
+
+}
