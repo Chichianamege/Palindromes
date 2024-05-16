2b991c2c0526b00e757e70af5cb614cc0bf35213
jake <130408715+Chichianamege@users.noreply.github.com>
2024-05-15 21:37:35 -0400

    Update dsassignment4part2.java

--- dsassignment4part2.java
+++ dsassignment4part2.java
@@ -1,8 +1,5 @@
 // Name: Chidera Anamege
-// Class: CS 3305/ W01
-// Term: Spring 2024
-//	Instructor:  Carla McManus
-//	Assignment:  04-Part-2-Palindromes
+
 
 import java.util.Scanner;
 import java.util.Stack;
